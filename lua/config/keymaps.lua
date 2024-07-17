f9e8b96c4b8c9bd45d736806995da7f66f2aa0c0
Rauliqbal <muhamadrauliqbal.gt@gmail.com>
2024-07-18 00:35:30 +0700

    [ADD] Lazyerm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Buffers keymap
+keymap("n", "<C-PageUp>", "<cmd>BufferLineMovePrev<cr>", opts)
+keymap("n", "<C-PageDown>", "<cmd>BufferLineMoveNext<cr>", opts)
+keymap("n", "<C-Left>", "<Esc>:bnext<CR>", opts)
+keymap("n", "<C-Right>", "<Esc>:bprevious<CR>", opts)
